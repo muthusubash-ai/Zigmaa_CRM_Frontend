ab99d230682839138bf2a37d8954a584a8a9423b
muthusubash.ai <mrprahsofficial@gmail.com>
2026-09-17 12:48:37 +0530

    auth permission

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,13 @@ import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: 5173,
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 });
+
